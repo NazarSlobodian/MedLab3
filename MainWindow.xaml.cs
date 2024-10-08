0c7f62015bbf94dc2327de1d070c7f3d81b16f35
NazarSlobodian <nazar.slobodian.pz.2022@lpnu.ua>
2024-10-08 21:07:27 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+namespace MedLab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //string str = ConfigurationManager.ConnectionStrings["connectionString"].ToString();
+            //MySqlConnection connection = new MySqlConnection(str);
+
+            //MySqlCommand cmd = new MySqlCommand("SELECT * FROM patients", connection);
+
+            //connection.Open();
+            //DataTable dataTable = new DataTable();
+            //dataTable.Load(cmd.ExecuteReader());
+            //connection.Close();
+            //dbTable.ItemsSource = dataTable.DefaultView;
+        }
+    }
+}
---
+++ Model/MedLabTypes/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedLab.Model.MedLabTypes
+{
+    public class Patient
+    {
+        public string FullName { get; set; }
+        public char Gender { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string ContactNumber { get; set; }
+        public string PatientPassword { get; set; }
+
+    }
+}
