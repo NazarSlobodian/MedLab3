f57999104c2514ce490e75724be17a758d204046
NazarSlobodian <nazar.slobodian.pz.2022@lpnu.ua>
2025-03-29 18:06:22 +0200

    Another reader

--- Model/Utils/Reader.cs
+++ Model/Utils/Reader.cs
@@ -36,7 +36,39 @@ namespace MedLab.Model.Utils
                     testTypes.Add(testType);
                 }
             }
+            List<TestNormalValues> normalValues = GetTestNormalValues();
+
             return testTypes;
+        }
+        public static List<TestNormalValues> GetTestNormalValues()
+        {
+            string filePath = "testNormalValues.txt";
+            List<TestNormalValues> testNormalValues = new List<TestNormalValues>();
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (string.IsNullOrWhiteSpace(line)) continue;
+                    line = line.Replace('.', ',');
+                    string[] parts = line.Split(" ");
+
+                    if (parts.Length != 6)
+                        throw new Exception("Wrong format");
+                    TestNormalValues testNormalValue = new TestNormalValues
+                    {
+                        TestNormalValuesID = int.Parse(parts[0]),
+                        MinAge =int.Parse(parts[1]),
+                        MaxAge = int.Parse(parts[2]),
+                        Gender = char.Parse(parts[3]),
+                        MinResValue = double.Parse(parts[4]),
+                        MaxResValue = double.Parse(parts[5]),
+                    };
+                    testNormalValues.Add(testNormalValue);
+                }
+            }
+            return testNormalValues;
         }
     }
 }
